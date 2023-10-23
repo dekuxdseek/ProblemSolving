50be50a125ac09c45abee91a3e70d14ceb92fd9d
dekuxdseek <swastiksharmaaa09@gmail.com>
2023-10-24 01:03:36 +0530

    .

---
+++ E_SleepingSchedule.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define vi vector<int>
+#define mii unordered_map<int, int>
+#define me(i, start, end) for(int i = start; i < end; ++i)
+#define she(i, start, end) for(int i = end-1; i >= start; --i)
+#define all(v) v.begin(), v.end()
+#define w(x) int x; cin >> x; while (x--)
+
+using namespace std;
+int mod = 1e9 + 7;
+
+
+int main() {
+    
+}  
