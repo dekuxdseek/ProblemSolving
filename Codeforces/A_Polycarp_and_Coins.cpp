444d5af0d2ca5e1cb78ea19aec1150d0a7771dd4
dekuxdseek <swastiksharmaaa09@gmail.com>
2023-11-06 23:53:52 +0530

    code

---
+++ Codeforces/A_Polycarp_and_Coins.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <list>
+#include <set>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <cstring>
+#include <cmath>
+#include <bitset>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstdlib>
+#include <iomanip>
+
+
+#define f                first
+#define s                second
+#define ll               long long
+#define pb               push_back
+#define mp               make_pair
+#define pii              pair<int,int>
+#define vi               vector<int>
+#define mii              unordered_map<int,int>
+#define pqb              priority_queue<int>
+#define pqs              priority_queue< int,vi,greater<int> >
+#define setbits(x)       __builtin_popcountll(x)
+#define zrobits(x)       __builtin_ctzll(x)
+#define mod              1000000007
+#define inf              1e10
+#define ps(x,y)          fixed<<setprecision(y)<<x
+#define mk(arr,n,type)   type *arr=new type[n];
+#define w(x)             int x; cin>>x; while(x--)
+#define me(i,start,end)  for(int i=start;i<end;i++)
+#define he(i,end,start) for(int i=end-1;i>=start;i--)
+#define all(v)         v.begin(),v.end()
+using namespace std;
+
+
+// Own The Journey
+
+
+// Finish it!!!
+
+
+// f->output, state, transition ,base-cases
+
+
+int main()
+{
+    // Your code goes here
+    w(t)
+    {
+        int n;
+        cin >> n;
+        int a, b;
+        
+        a = n/3;
+        b = n/3;
+        if(n%3 == 1)++a;
+        else if(n%3 == 2) ++b;
+        cout << a <<" "<< b << endl;
+    }
+    return 0;
+}
